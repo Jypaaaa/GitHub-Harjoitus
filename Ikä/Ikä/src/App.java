dc9761b01b3a4e63ef3e8a2b21db4177dba784d1
Jypaaaa <amk1006156@student.hamk.fi>
2024-10-19 18:47:18 +0300

    IkäOhjelma

---
+++ Ikä/Ikä/src/App.java
@@ -0,0 +1,44 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int ika = 65;
+        if (ika > 0 && ika < 18) {
+            System.out.println("Olet alaikäinen");
+        
+            if (ika >= 15) {
+                System.out.println("Saat ajaa mopoa");
+            }
+        
+            if (ika >= 16 && ika < 18) {
+                System.out.println("Saat ajaa kevaria");
+            }
+        } else if (ika == 18) {
+            System.out.println("Olet juuri tullut täysi-ikäiseksi ja saat ajaa autoa");
+        } else if (ika >= 65) {
+            System.out.println("Olet eläkeläinen");
+            if (ika == 65) {
+                System.out.println("Hyviä eläkepäiviä!");
+            }
+        } else {
+            System.out.println("Olet aikuinen");
+        
+            if (ika % 10 == 0) {
+                System.out.println("Onnittelut tasavuosikymmenistä!");
+            }
+        
+            if (ika == 100) {
+                System.out.println("Paljon onnea!");
+                System.out.println("Olet saavuttanut 100 vuoden iän!");
+                System.out.println("Toivotamme sinulle kaikkea hyvää!");
+            }
+        
+            if (ika >= 58) {
+                System.out.println("Voit mennä varhaiseläkkeelle");
+            }
+        
+            if (ika >= 40 && ika <= 50) {
+                System.out.println("Parasta keski-ikää!");
+            }
+        }
+        
+    }
+}
